189d386d1577073ad4bf8cbc7a8432dbe089ff54
SqueakyParr0t <117523718+VoidParr0t@users.noreply.github.com>
2023-05-05 14:55:56 -0400

    Normal Commit

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'f40ce30f-abf9-4ed5-bf9b-944ada950fb1'
 // SEE NOTE ABOVE.
 
 
